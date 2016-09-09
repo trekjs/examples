7147fd963b00a0ad55c431327343b9018f6ca718
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-09-09 16:19:28 +0800

    pkg: trek-view-marko v0.0.3

--- template-marko/app.js
+++ template-marko/app.js
@@ -4,12 +4,12 @@ const Engine = require('trek-engine')
 
 const marko = require('trek-view-marko')
 
-marko.install()
+marko.install(__dirname)
 
 const app = new Engine()
 
 app.use(({ res }) => {
-  res.send(200, marko.render(path.join(__dirname, './index.marko'), { name: 'Trek Engine' }))
+  res.send(200, marko.render('index.marko', { name: 'Trek Engine' }))
 })
 
 app.run(3000)
