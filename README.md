2baa77581d67b3ea48e7fa89c943404ba7995749
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-09-09 13:28:13 +0800

    examples: add template-pug

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 * [hello-world](hello-world) - hello world application
 * [template-marko](template-marko) - view rending with marko
+* [template-pug](template-pug) - view rending with pug(jade)
---
+++ template-pug/app.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+const Engine = require('trek-engine')
+
+const pug = require('trek-view-pug')
+
+const app = new Engine()
+
+app.use(({ res }) => {
+  pug.render(path.join(__dirname, './index.pug'), { name: 'Trek Engine' })
+    .then(html => res.send(200, html))
+})
+
+app.run(3000)
